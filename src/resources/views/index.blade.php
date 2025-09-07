da244a1a5e332938361b973bd003925ff0ad7ce7
gotouge <gotouge@gmail.com>
2025-09-07 22:04:49 +0900

    トップページのレイアウト調整（仮）

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,26 +6,31 @@
 
 @section('content')
 <div class="flex__item">
-    @foreach($items as $item)
-    <div class="card">
-        <a href="{{ route('item',['id' => $item->id]) }}">
+    <div class="flex__header">
+        <h2 class="flex__header-ttl">商品一覧</h2>
+        <div class="flex__header__create-button">
+            <a href="/item" class="create-button__link">購入に進む</a>
+        </div>
+
+    </div>
+    <div class="contents">
+        <div class="contents__left">
+            検索欄とカテゴリ選択
+        </div>
+        <div class="card">
 
             <div class="card__image">
                 <!-- imageのurl指定 -->
-                <img src="{{ asset('storage/' . $item->image_url)}}" />
             </div>
-        </a>
-        <div class="card__content">
-            <div class="card__content-common">
-                <div class="card__price">¥{{ $item->price }}</div>
-                <div class="card__buy">
-                    <!-- <a href="/item" class="card__link">購入に進む</a> -->
-                    <a href="{{ route('item',['id' => $item->id]) }}" class="card__link">購入に進む</a>
+            <!-- </a> -->
+            <div class="card__content">
+                <div class="card__content-common">
+                    <div class="card__buy">
+                    </div>
                 </div>
             </div>
-            <h2 class="card__ttl">{{ $item->name }}</h2>
         </div>
+
     </div>
-    @endforeach
 </div>
 @endsection
