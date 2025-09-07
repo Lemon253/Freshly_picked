e27fe827e930e5f5d728396c98aed416d102b2f7
gotouge <gotouge@gmail.com>
2025-09-07 17:04:12 +0900

    モデル作成 ・Product ・Season

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Season;
 
 class ItemController extends Controller
 {
     //
     public function index()
     {
-        return view('index');
+        // 1ページ7件ずつ表示
+        $products = Product::with('seasons')->paginate(7);
+        return view('index', compact('products'));
     }
 }
