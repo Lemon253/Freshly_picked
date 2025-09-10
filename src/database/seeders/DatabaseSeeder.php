511309c578d7be3e1c72a1eb5c47910060366534
gotouge <gotouge@gmail.com>
2025-09-11 06:34:38 +0900

    中間テーブル用のダミーデータ作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SeasonsTableSeeder::class,
             ProductsTableSeeder::class,
+            ProductSeasonTableSeeder::class,
         ]);
     }
 }
