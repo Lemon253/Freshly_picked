b793b559ee21baad7684d28771a3b449a0743140
gotouge <gotouge@gmail.com>
2025-09-15 05:26:11 +0900

    ルートの修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,21 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', [ItemController::class, 'index'])->name('products.index');
+
+Route::get('/products', [ItemController::class, 'index'])->name('products.index');
 Route::get('/products/search', [ItemController::class, 'search'])->name('items.search');
-Route::get('/items/{id}', [ItemController::class, 'item'])->name('item');
-Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update');
-Route::delete('/items/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
+Route::get('/products/{id}', [ItemController::class, 'item'])->name('item');
+Route::put('/products/{id}/update', [ItemController::class, 'update'])->name('items.update');
+Route::delete('/products/{id}/delete', [ItemController::class, 'destroy'])->name('items.destroy');
 Route::get('/register', [ItemController::class, 'register']);
 Route::post('/register', [ItemController::class, 'store'])->name('items.store');
