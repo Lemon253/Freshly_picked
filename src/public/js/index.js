9914ef41f28fe566e59b020dab64606c680f8a52
gotouge <gotouge@gmail.com>
2025-09-20 12:30:44 +0900

    セレクトボックス選択後に検索も行うように再修正

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -6,12 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
     selectElement.addEventListener('change', function() {
         // 現在の検索キーワードをdata属性から取得
         const searchTerm = form.getAttribute('data-search-term');
-
-        // 検索キーワードがない場合にのみ、検索欄を無効化
-        if (!searchTerm) {
-            searchInput.removeAttribute('name');
-        }
-
         // フォームを送信
         form.submit();
     });
